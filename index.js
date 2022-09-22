68e7e8c05f1eb02c4fcf0994303b978517e16846
Arturo Torres Flores <110277183+Arturo9314@users.noreply.github.com>
2022-09-22 06:06:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+(() => {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+    const name = document.getElementById('validationServer01')
+    const lastname = document.getElementById('validationServer02')
+    const email = document.getElementById('validationServer03')
+    const password = document.getElementById('validationServer04')
+    console.log(email);   // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+            name.setAttribute('placeholder', "")
+            lastname.setAttribute('placeholder', "")
+            email.setAttribute('placeholder', "email@example/com")
+            password.setAttribute('placeholder', "")
+        }, false)
+        })
+})()
